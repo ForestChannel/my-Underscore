72be7bea0c5d81a4eda2cb1ea82f2fdd88a312e9
ForestChannel <vip.golovanets@gmail.com>
2021-08-17 00:59:59 +0300

    first commit

---
+++ modules.js
@@ -0,0 +1,19 @@
+let _ = {
+  each: function (args, cb) {
+    if (Array.isArray(args)) {
+      let i = 0
+      for (const iterator of args) {
+        cb(iterator, i)
+        i++
+      }
+    } else {
+      for (const key in args) {
+        if (Object.hasOwnProperty.call(args, key)) {
+          const element = args[key]
+          cb(element, key)
+        }
+      }
+    }
+  },
+  map: function (args, cb) {},
+}
---
+++ usage.js
@@ -0,0 +1,8 @@
+////// Each Alias forEach
+
+// let $ = [1, 2, 3, 4, 5]
+// let $ = {
+//   one: 1,
+//   two: 2,
+// }
+// _.each($, (element, iterator) => console.log(`${iterator} : ${element}`))
