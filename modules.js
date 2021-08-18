e4bb5d2b653f1caeadf73b395338face65f89074
ForestChannel <vip.golovanets@gmail.com>
2021-08-18 17:00:43 +0300

    map done

--- modules.js
+++ modules.js
@@ -1,3 +1,9 @@
+// let $ = [1, 2, 3, 4, 5]
+
+let $ = {
+  one: 1,
+  two: 2,
+}
 let _ = {
   each: (args, cb) => {
     if (Array.isArray(args)) {
@@ -15,5 +21,28 @@ let _ = {
       }
     }
   },
-  map: (args, cb) => {},
+  map: (args, cb) => {
+    let i = 0
+    if (Array.isArray(args)) {
+      for (const iterator of args) {
+        args[i] = cb(iterator)
+        i++
+      }
+      return args
+    } else{
+      let arr = []
+      for (const key in args) {
+        if (Object.hasOwnProperty.call(args, key)) { 
+          const element = args[key];
+          arr[i] = cb(element)
+          i++
+        }
+      }
+      return arr 
+    }
+  },
+  reduce: (args, cb) => {
+
+  },
 }
+var sum = _.reduce($, (memo, num) => memo + num, 0)
